01291014f54e3c994a35ba823adfc63d0d83effd
Rushiraj Kudapane <kudapanerushi13@gmail.com>
2024-03-04 00:32:03 +0530

    change app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT | 5001;
 //   optionsSuccessStatus: 204,
 // };
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json()); //body parser for request bodies
 app.use("/api", router); // for route /api router will be called
 app.use("/api/enquiry", enquiryRouter);
